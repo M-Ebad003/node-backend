3cab46b47f43d123f7aa2bc5818ddddd943da00b
ebad <ebadmuhammad003@gmail.com>
2025-01-19 21:25:44 +0500

    fix

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 
+const userData={
+  'name' : 'ebad',
+  'Gender': "Male",
+  "Age" : "21 years old",
+  "Semester": '5th'
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -12,4 +19,7 @@ app.get('/login', (req,res)=>{
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+app.get('/github', (req,res)=> {
+  res.json(userData)
 })
